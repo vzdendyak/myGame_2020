81541c7d91e20c009d8367cd32bc9904bb94ceb9
ihyavorskyi <ihor201324@gmail.com>
2020-10-25 13:08:30 +0200

    Commit

--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -1,32 +1,6 @@
-/****************************************************************************
- Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
-
- http://www.cocos2d-x.org
-
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
-
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
-
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
- ****************************************************************************/
-
 #ifndef __HELLOWORLD_SCENE_H__
 #define __HELLOWORLD_SCENE_H__
-#include "ui/CocosGUI.h";
 #include "cocos2d.h";
-
 class HelloWorld : public cocos2d::Layer
 {
 public:
@@ -41,9 +15,9 @@ private:
 	cocos2d::JumpBy* jumpBy;
 	cocos2d::Animate* animate;
 	cocos2d::Animate* animateJump;
-	cocos2d::Animate* animateDown;
+	cocos2d::Animate* animateFastMove;
 	cocos2d::SpriteFrame* animateStop;
 
-	bool r_move = false, l_move = false, b_move = false, isMove = false;
+	bool r_move = false, l_move = false, isMove = false;
 };
 #endif // __HELLOWORLD_SCENE_H__
